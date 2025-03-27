47eaeabb06c890d823b60a58a659311bbd3c4029
Ruziev Asilbek <roziyev106@gmail.com>
2025-03-27 21:35:53 +0500

    Make feature tabs work

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const elsTabItem = document.querySelectorAll('.tabs__item')
+const elsTabPanel = document.querySelectorAll('.tabs__panel')
+const elsTabLink = document.querySelectorAll('.js-tabs-link')
+
+
+function deactivateTabItems () {
+
+  elsTabItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  })
+}
+
+function deactivateTabPanels () {
+
+  elsTabPanel.forEach(function (elTabsPanel) {
+    elTabsPanel.classList.remove('tabs__panel--active');
+  })
+}
+
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    // Prevent page move
+    evt.preventDefault();
+
+    // Remove active class form tabs__item element
+    deactivateTabItems ();
+
+    // Add active class to current tabs__item
+    elTabLink.parentElement.classList.add('tabs__item--active');
+
+    // Remove active class from tabs__panels element
+    deactivateTabPanels();
+
+    // Show active tab panel
+    /* const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`); */
+    const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+    elTargetPanel.classList.add('tabs__panel--active');
+  })
+
+})
